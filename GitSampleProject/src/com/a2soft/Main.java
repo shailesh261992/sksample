b59ba07ed281935be4452917490f3b4670ea379e
Shailesh Kadam <shailesh2619922@gmail.com>
2016-01-24 19:15:28 +0530

    commit 3 - branch2

--- GitSampleProject/src/com/a2soft/Main.java
+++ GitSampleProject/src/com/a2soft/Main.java
@@ -5,5 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hi shailesh");
 		System.out.println("Hi tejas");
+		System.out.println("Hi kadam tejas");
+		
+		System.out.println("Hi yedya");
 	}
 }
